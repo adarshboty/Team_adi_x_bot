a223f1acda212137a3981a5905ec03f9bde398ef
adarshboty <aadarshbehera68@gmail.com>
2025-08-05 02:43:57 +0530

    Update Coinfig.py

--- Coinfig.py
+++ Coinfig.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8478205342:AAHBS3ZpFFtCcOSHL_nE5LNu8AtcMMuajPg")
 BIO_LINKS = ['linktr.ee', 'beacons.ai', 'campsite.bio', 'solo.to', 'bio.site']
 ADMIN_IDS = [123456789]  # Telegram IDs of admins who bypass
 SUPPORT_URL = "https://t.me/bollywoodmusicsupport1"
