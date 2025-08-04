6b61fc1a03fdbbc239dee15c67528592e945b4d5
adarshboty <aadarshbehera68@gmail.com>
2025-08-05 02:33:40 +0530

    Create Coinfig.py

---
+++ Coinfig.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BIO_LINKS = ['linktr.ee', 'beacons.ai', 'campsite.bio', 'solo.to', 'bio.site']
+ADMIN_IDS = [123456789]  # Telegram IDs of admins who bypass
+SUPPORT_URL = "https://t.me/bollywoodmusicsupport1"
+FOUNDER_URL = "https://t.me/adityathedevloper"
